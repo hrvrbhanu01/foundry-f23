b69e67051a1c5d0235bbfff0d75254c12a7741f6
Bhanu Prakash Dixit <74755575+hrvrbhanu01@users.noreply.github.com>
2024-07-30 11:42:31 +0530

    written Counter.s.sol (script) file

--- foundry-simple-storage-f23/script/Counter.s.sol
+++ foundry-simple-storage-f23/script/Counter.s.sol
@@ -1,19 +1,20 @@
 // SPDX-License-Identifier: UNLICENSED
+
+//This is a script for deploying our smart-contract inside test src directory:
+//vm.  is only used in foundry (cheatcode)
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
 import {Counter} from "../src/Counter.sol";
 
 contract CounterScript is Script {
-    Counter public counter;
-
-    function setUp() public {}
-
-    function run() public {
+    function run() external returns(Counter) {
         vm.startBroadcast();
 
-        counter = new Counter();
+        Counter counter = new Counter();
 
         vm.stopBroadcast();
+        return counter;
     }
+
 }
